ae25d8c664abc692082f9cb3f26707542de7bcea
Asutosh Pradhan <75766416+Maagztech@users.noreply.github.com>
2022-10-14 17:36:04 +0530

    implementation of express rate lomiter

--- server/index.ts
+++ server/index.ts
@@ -7,9 +7,18 @@ import morgan from "morgan";
 import routes from "./routes/index";
 import { createServer } from "http";
 import { Server, Socket } from "socket.io";
+import rateLimit from "express-rate-limit";
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
 // Middleware
 const app = express();
+app.use(limiter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
